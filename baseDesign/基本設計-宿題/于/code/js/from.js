cb708cea267dec9802daa677bedd9ced97020573
Agile-Y <igrs0106@yahoo.co.jp>
2020-12-07 17:46:19 +0900

    2020-12-07-upData

---
+++ baseDesign/基本設計-宿題/于/code/js/from.js
@@ -0,0 +1,31 @@
+layui.use('table', function(){
+    let table = layui.table;
+    
+    
+    table.render({
+      elem: '#record'
+      ,height: 330
+      ,url: '/jsonData/testData.json' //データの保存先
+      ,page: true 
+      ,cols: [[ 
+        {field: 'id', title: 'ID', width:80, sort: true, fixed: 'left'}
+        ,{field: 'username', title: '取引先名', width:150}
+        ,{field: 'customerNameKana', title: '取引先名カナ', width:190, sort: true}
+        ,{field: 'bp', title: 'BP', width:120} 
+        ,{field: 'streetAddress', title: '住所', width: 200}
+        ,{field: 'tel', title: 'Tel', width: 150, sort: true}
+        ,{field: 'mail', title: 'Mail', width: 150, sort: true}
+        ,{field: 'businessField', title: '業務分野', width: 120}
+        ,{field: 'businessContent', title: '業務内容', width: 135, sort: true}
+        ,{field: 'remarks', title: '備考', width:80}
+        ,{field: 'responsibleParty', title: '担当者', width:80}
+        ,{field: 'createdDate', title: '作成日', width:80}
+        ,{field: 'author', title: '作成者', width:80}
+        ,{field: 'updateDate', title: '更新日', width:80}
+        ,{field: 'changer', title: '更新者', width:80}
+        ,{field: 'operation', title: '操作', width:80}
+
+      ]]
+    });
+    
+  });
