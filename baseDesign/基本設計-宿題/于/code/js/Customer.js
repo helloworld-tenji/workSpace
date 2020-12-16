6579f83b1a3edb4da3ad7f4055e5a63d43052581
Agile-Y <igrs0106@yahoo.co.jp>
2020-12-16 18:42:04 +0900

    2020-12-16-upData

--- baseDesign/基本設計-宿題/于/code/js/Customer.js
+++ baseDesign/基本設計-宿題/于/code/js/Customer.js
@@ -19,23 +19,13 @@ layui.use('laydate', function(){
       ,url: '../jsonData/testData.json' //データの保存先
       ,page: true 
       ,toolbar: '#toolbarDemo'
-      ,cols: [[ 
-        {field: 'id', title: 'ID', width:80, sort: true, fixed: 'left'}
-        ,{field: 'username', title: '取引先名', width:150}
-        ,{field: 'customerNameKana', title: '取引先名カナ', width:190, sort: true}
-        ,{field: 'bp', title: 'BP', width:120} 
-        ,{field: 'streetAddress', title: '住所', width: 200}
-        ,{field: 'tel', title: 'Tel', width: 150, sort: true}
-        ,{field: 'mail', title: 'Mail', width: 150, sort: true}
-        ,{field: 'businessField', title: '業務分野', width: 120}
-        ,{field: 'businessContent', title: '業務内容', width: 135, sort: true}
-        ,{field: 'remarks', title: '備考', width:80}
-        ,{field: 'responsibleParty', title: '担当者', width:80}
-        ,{field: 'createdDate', title: '作成日', width:80}
-        ,{field: 'author', title: '作成者', width:80}
-        ,{field: 'updateDate', title: '更新日', width:80}
-        ,{field: 'changer', title: '更新者', width:80}
-        ,{field: 'operation', title: '操作', width:120,toolbar:'#iconDemo' }
+      ,cols: [[
+          {title: '第１四半期', align: 'center', colspan: 5 }
+      ],
+        [ {field: 'id', title: '月', width:80, sort: true, fixed: 'left'}
+        ,{field: 'username', title: '現状', width:200}
+        ,{field: 'customerNameKana', title: '目標', width:270,height:20, sort: true}
+        ,{field: 'operation', title: '実施', width:200,toolbar:'#iconDemo' }
       ]]
     });
     
